953b10c330f0c163a7d2e4230d751a73427c8fe5
cd20202 <cd20202@live.com>
2015-11-12 14:58:44 -0800

    updated user controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,16 +17,25 @@ class UsersController < ApplicationController
 		redirect_to '/users/#{@user.id}'
 	end
 	def edit
-
+		id = params[:id]
+		@user = User.find(id)
+		render :edit
 	end
 	def update
-
+		id = params[:id]
+		user = User.find(id)
+		updated_info = params.require(:user).permit(:user_name, :current_city, :about)
+		user.update_attributes(updated_info)
+		redirect_to '/users/#{id}'
 	end
 	def show
 		@user = User.find(params[:id])
 		render :show
 	end
 	def destroy
-		
+		id = params[:id]
+		user = User.find(id)
+		user.destroy
+		redirect_to '/users'
 	end
 end
