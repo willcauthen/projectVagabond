b669b4aa7645529974241acb55d6cf821aebb599
cd20202 <cd20202@live.com>
2015-11-12 14:46:12 -0800

    updated user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,32 @@
 class UsersController < ApplicationController
 	def index
+		@users = User.all
+		@posts = @user.post.all
+		render :index
 	end
+
 	def new
+		@user = User.new
+		render :new
 	end
+
 	def create	
+		user_params = params.require(:user).permit(:user_name, :email, :password, :current_city, :about)
+		@user = User.create(user_params)
+		login(@user)
+		redirect_to '/users/#{@user.id}'
 	end
-	def edit	
+	def edit
+
 	end
 	def update
+
 	end
 	def show
+		@user = User.find(params[:id])
+		render :show
 	end
 	def destroy
+		
 	end
 end
