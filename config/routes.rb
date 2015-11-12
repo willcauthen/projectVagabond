c72e14913f2e870dc93fbbd2c898a0efabb485c7
Will Cauthen <willcauthen50@gmail.com>
2015-11-12 13:33:02 -0800

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "/posts", to: "posts#index" 
+  
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +56,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
