b2598898e0581122ca82b98c903e2d1144542c65
Will Cauthen <willcauthen50@gmail.com>
2015-11-13 11:35:34 -0800

    routes and stuff

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,6 @@
 class Post < ActiveRecord::Base
-<<<<<<< HEAD
 	has_many :post_users
 	has_many :users, through: :post_users
-=======
 	belongs_to :user
-	has_many :user
->>>>>>> fc4cef16423202a415645f7baf2d193ac3ac9764
+	# has_many :user
 end
