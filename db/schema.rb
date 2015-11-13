2b8950e8887223617f7fba4c28c4d2f2c3b0c9fb
Uriel <uriel.chavez@gmail.com>
2015-11-12 20:36:29 -0800

    city controllers, models, need second eyes for correct associations between city-user-post

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112233104) do
+ActiveRecord::Schema.define(version: 20151113013843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.float    "lat"
+    t.float    "lng"
+    t.string   "posts"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_users", force: :cascade do |t|
     t.integer  "user_id"
