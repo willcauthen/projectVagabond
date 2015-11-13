4b472e4a4df13ee505642c980f631c708c030ea4
Uriel <uriel.chavez@gmail.com>
2015-11-13 11:31:30 -0800

    test

--- config/routes.rb
+++ config/routes.rb
@@ -10,20 +10,20 @@ Rails.application.routes.draw do
   	resources :posts
   end
 
-  get "/sign-in", to: "sessions#new"
-  post "/sessions", to: "sessions#create"
+  # get "/sign-in", to: "sessions#new"
+  # post "/sessions", to: "sessions#create"
 
-  get "/users", to: "users#index", as: "users"
-  get "/posts", to: "posts#index", as: "posts"
+  # get "/users", to: "users#index", as: "users"
+  # get "/posts", to: "posts#index", as: "posts"
 
-  get "/cities", to: "cities#index"
-  get "/cities/new", to: "cities#new", as: "new_city"
+  # get "/cities", to: "cities#index"
+  # get "/cities/new", to: "cities#new", as: "new_city"
 
-  post "/cities", to: "cities#create"
+  # post "/cities", to: "cities#create"
 
-  get "/users/:user_id/cities", to: "city_users#index", as: "user_cities"
-  # I believe we also need a get/and post posts for the cities
-  post "/cities/:city_id/users", to: "city_users#create", as: "city_users"
+  # get "/users/:user_id/cities", to: "city_users#index", as: "user_cities"
+  # # I believe we also need a get/and post posts for the cities
+  # post "/cities/:city_id/users", to: "city_users#create", as: "city_users"
 
 end
 
--- db/migrate/20151113014848_create_city_users.rb
+++ db/migrate/20151113014848_create_city_users.rb
@@ -3,7 +3,7 @@ class CreateCityUsers < ActiveRecord::Migration
     create_table :city_users do |t|
       t.references :user, index: true, foreign_key: true
       t.references :post, index: true, foreign_key: true
-      t.reference :city
+      t.references :city
 
       t.timestamps null: false
     end
