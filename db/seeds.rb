9e90ef51e99d9e23dae9bf81b07257aa61f30d59
Laulaucia <buitrago.laura@gmail.com>
2015-11-16 18:46:59 -0800

    changed some things on the landing page and some styling and titles for posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,115 +30,115 @@ User.create(user_name: 'mojojoejoe',
 			password: 'joejoejoe',
 			current_city: 'New York',
 			about: 'Phil Collins understands me')
-City.create(name: 'San Francisco, CA',
+City.create(name: 'San Francisco',
 			lat: 37.790,
 			lng: -122.403,
 			CityImg: 'SanFrancisco')
-City.create(name: 'Cleveland, OH',
+City.create(name: 'Cleveland',
 			lat: 41.506,
 			lng: -81.701,
 			CityImg: 'Cleveland')
 
-City.create(name: 'New York, NY',
+City.create(name: 'New York',
 			lat: 40.689,
 			lng: -74.046,
 			CityImg: 'NewYork')
-City.create(name: 'Boston, MA',
+City.create(name: 'Boston',
 			lat: 42.346,
 			lng: -71.099,
 			CityImg: 'Boston')
 
-City.create(name: 'London, UK',
+City.create(name: 'London',
 			lat: 51.501,
 			lng: -0.144,
 			CityImg: 'London')
 
 
-Post.create(city: 'San Francisco, CA',
-			title: 'Post1',
+Post.create(city: 'San Francisco',
+			title: 'Bart is good with wheelchairs',
 			user: "1",
 			content: 'My name is Wilbo, and I am limited to a wheelchair. The BART rail system in San Francisco has good accessibility for those with similar mobility restrictions.')
-Post.create(city: 'San Francisco, CA',
-			title: 'Post2',
+Post.create(city: 'San Francisco',
+			title: 'GG Bridge is a farce',
 			user: "1",
 			content: 'The Golden Gate bridge is not made of gold.  Talk about bait and switch.')
-Post.create(city: 'San Francisco, CA',
-			title: 'Post2',
+Post.create(city: 'San Francisco',
+			title: 'Palace of Fine Arts',
 			user: "1",
 			content: 'Palace of fine arts is beautiful.  No art though.')
-Post.create(city: 'San Francisco, CA',
-			title: 'Post2',
+Post.create(city: 'San Francisco',
+			title: 'GA ',
 			user: "2",
 			content: 'The building at 225 Bush Street is not only lovely beyond compare, but also has a totally baller assistive listening system available.')
-Post.create(city: 'Cleveland, OH',
-			title: 'Post3',
+Post.create(city: 'Cleveland',
+			title: 'Yumm, treats',
 			user: "1",
 			content: 'Cousin Bubba was right, the squirrel here is delish')
-Post.create(city: 'Cleveland, OH',
-			title: 'Post1',
+Post.create(city: 'Cleveland',
+			title: 'The Browns',
 			user: "2",
 			content: 'The whole town supports the Browns!!! Everything is covered in brown stuff.')
-Post.create(city: 'Cleveland, OH',
-			title: 'Post2',
+Post.create(city: 'Cleveland',
+			title: 'Education',
 			user: "2",
 			content: "I can't tell if the elementary school doubles for the university, but they both have lovely swings.")
-Post.create(city: 'Cleveland, OH',
-			title: 'Post3',
+Post.create(city: 'Cleveland',
+			title: 'The Fairmont',
 			user: "2",
 			content: 'Can you believe there is a Fairmont Hotel in Cleveland???!!!')
-Post.create(city: 'Cleveland, OH',
-			title: 'Post1',
+Post.create(city: 'Cleveland',
+			title: 'Browns 2',
 			user: "3",
 			content: 'Did I mention that the Browns play here? I can only say that for another year.')
-Post.create(city: 'Cleveland, OH',
-			title: 'Post2',
+Post.create(city: 'Cleveland',
+			title: 'Browns again',
 			user: "3",
 			content: 'I did not realize that the Browns had a smell.  Someone needs to flush.')
-Post.create(city: 'San Francisco, CA',
-			title: 'Post3',
+Post.create(city: 'San Francisco',
+			title: 'The Castro for shows',
 			user: "3",
 			content: 'The Rusty Nipple, a burlesque show in The Castro, is not very good for the visually impaired. They gave me a hard time about my seeing-eye dog and tried to refuse us entry.')
-Post.create(city: 'San Francisco, CA',
-			title: 'Post1',
+Post.create(city: 'San Francisco',
+			title: 'Sam Wo',
 			user: "4",
 			content: 'Sam Wo is everything Conan said it would be, and more!!!')
-Post.create(city: 'San Francisco, CA',
-			title: 'Post1',
+Post.create(city: 'San Francisco',
+			title: 'The San Francisco',
 			user: "4",
 			content: 'The San Francisco was a great place, my son, who lives with ASD, had a great time looking at the exhibits.')
-Post.create(city: 'San Francisco, CA',
-			title: 'Post1',
+Post.create(city: 'San Francisco',
+			title: 'Jameyster',
 			user: "4",
 			content: 'Try the Jameyster app, best tourguide of SF!!!')
-Post.create(city: 'New York, NY',
-			title: 'Post2',
+Post.create(city: 'New York',
+			title: 'Ipsums',
 			user: "4",
 			content: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.')
-Post.create(city: 'New York, NY',
+Post.create(city: 'New York',
 			title: 'Post3',
 			user: "4",
 			content: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt.')
-Post.create(city: 'New York, NY',
+Post.create(city: 'New York',
 			title: 'Post1',
 			user: "5",
 			content: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt.')
-Post.create(city: 'New York, NY',
+Post.create(city: 'New York',
 			title: 'Post2',
 			user: "5",
 			content: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt.')
-Post.create(city: 'London, UK',
+Post.create(city: 'London',
 			title: 'Post3',
 			user: "5",
 			content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae.')
-Post.create(city: 'London, UK',
+Post.create(city: 'London',
 			title: 'Post3',
 			user: "2",
 			content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae.')
-Post.create(city: 'London, UK',
+Post.create(city: 'London',
 			title: 'Post3',
 			user: "1",
 			content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae.')
-Post.create(city: 'London, UK',
+Post.create(city: 'London',
 			title: 'Post3',
 			user: "5",
 			content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae.')
